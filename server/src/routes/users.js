00060a3253fbf8ce0eebcdcdaac127b6cf1d9481
bijoy-v <bijoyyv@gmail.com>
2024-02-29 21:48:46 +0530

    first commit

---
+++ server/src/routes/users.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const router = express.Router()
+
+
+
+router.post("/register",async (req,res)=>{
+    const {username,password} = req.body;
+
+    const user = await userModel.findOne({username});
+
+    res.json(user);
+})
+
+router.post("/login")
+
+
+
+
+
+
+
+
+
+export { router as userRouter };
