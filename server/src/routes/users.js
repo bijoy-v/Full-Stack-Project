1850a370affed63f71038c6dcc28d14e94c905c4
bijoy-v <bijoyyv@gmail.com>
2024-02-29 22:48:15 +0530

    second commit

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -11,10 +11,33 @@ router.post("/register",async (req,res)=>{
 
     const user = await userModel.findOne({username});
 
-    res.json(user);
+    if (user) {
+        return res.json({message:"user already exisist"})
+    }
+const hashedPassword = await bcrypt.hash(password,10)
+
+const newUser = new UserModel({username,password:hashedPassword});
+await newUser.save();
+
+
+    res.json({message:"user Registered Successfully"});
 })
 
-router.post("/login")
+router.post("/login", async (req,res)=>{
+    const {username,password} = req.body;
+
+    const user = await userModel.findOne({username});
+    if(!user){
+        return res.json({message:"User Dosen't Exist"})
+    }
+    const isPasswordVaild = await bcrypt.compare(password,user.password)
+    if(!isPasswordVaild){
+        return res.json({message:"Username or Password is incorrect"})
+    }
+    const token =  jwt.sign({id:user._id},"secret")
+    res.json({token,userID: user._id })
+
+})
 
 
 
